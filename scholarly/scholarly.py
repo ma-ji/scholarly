ce4875a4863af80215590f4afd6fa4fb7fe42529
Ji Ma <11808231+ma-ji@users.noreply.github.com>
2020-04-12 15:56:30 -0500

    Update scholarly.py

--- scholarly/scholarly.py
+++ scholarly/scholarly.py
@@ -78,7 +78,7 @@ def _get_page(pagerequest):
     """Return the data for a page on scholar.google.com"""
     # Note that we include a sleep to avoid overloading the scholar server
     time.sleep(5+random.uniform(0, 5))
-    resp = _SESSION.get(pagerequest, headers=_HEADERS, cookies=_COOKIES)
+    resp = _SESSION.get(pagerequest, headers=_HEADERS, cookies=_COOKIES, timeout=3)
     if resp.status_code == 200 and 'gs_captcha_f' not in resp.text:
         # Valid request: with or without search results, but without authenticiation.
         # Situations does work:
